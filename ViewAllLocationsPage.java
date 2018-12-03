dff6ee8ed7e7b393ddff3e44a07947d0b9254dee
cking90 <cking90@gatech.edu>
2018-12-03 14:19:08 -0500

    View all locations

--- ViewAllLocationsPage.java
+++ ViewAllLocationsPage.java
@@ -3,20 +3,41 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
+import java.util.Set;
 
 public class ViewAllLocationsPage extends JFrame implements ActionListener {
     private Container north;
     private JLabel title;
     private JPanel titlePanel;
     private JPanel buttonPanel;
+    private JPanel tablePanel;
 
     private JButton back;
 
 
     ViewAllLocationsPage() {
+        Set<Location> locations = Location.getLocationList();
+        Object[][] data = new Object[locations.size()][3];
+        int i = 0;
+        System.out.println(Location.locationListToString());
+        for (Location l: locations) {
+            data[i][0] = l.getName();
+            data[i][1] = l.getCoordinates();
+            data[i][2] = l.getType();
+            i++;
+        }
+        String[] labels = {"Name", "Coordinates", "Type"};
+        JTable table = new JTable(data, labels);
+        table.setRowHeight(30);
+        table.setRowHeight(20, 60);
+
+        tablePanel = new JPanel();
+        tablePanel.add(table);
+
         titlePanel = new JPanel();
         title = new JLabel("All Locations");
         titlePanel.add(title);
+
 
         buttonPanel = new JPanel();
         back = new JButton("Back");
@@ -28,6 +49,7 @@ public class ViewAllLocationsPage extends JFrame implements ActionListener {
         north = getContentPane();
         north.setLayout(new BoxLayout(north, BoxLayout.Y_AXIS));
         north.add(titlePanel);
+        north.add(tablePanel);
         north.add(buttonPanel);
 
         north.setVisible(true);
